cc3a175331205c577afe4b5e964f8968a5463c27
MartinAldkdkdj <65997389+MartinAldkdkdj@users.noreply.github.com>
2022-02-22 21:48:27 -0600

    Add files via upload

---
+++ metodo numericos.py
@@ -0,0 +1,31 @@
+from math import factorial
+
+x=-5
+y=1
+contador = 1
+while contador < 11:
+    y=(x**contador)/factorial(contador)+y
+    contador+=1
+z=1/y
+print (y)
+print (z)
+print(" ")
+x=-5
+y=1
+contador = 1
+while contador < 21:
+    y=(x**contador)/factorial(contador)+y
+    contador+=1
+z=1/y
+print (y)
+print (z)
+print(" ")
+x=-5
+y=1
+contador = 1
+while contador < 31:
+    y=(x**contador)/factorial(contador)+y
+    contador+=1
+z=1/y
+print (y)
+print (z)
